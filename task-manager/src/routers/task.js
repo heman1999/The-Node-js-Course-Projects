c68bf6a774fcc6c7404abbd84ec722bfd5df78c4
Himanshu Upadhyay <heman6760@gmail.com>
2020-09-26 18:37:54 +0530

    refactored code

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -18,9 +18,29 @@ router.post("/tasks", auth, async (req, res) => {
 });
 
 router.get("/tasks", auth, async (req, res) => {
+  const match = {};
+  const sort = {};
+  if (req.query.completed) {
+    match.completed = req.query.completed === "true";
+  }
+
+  if (req.query.sortBy) {
+    const part = req.query.sortBy.split(":");
+    sort[part[0]] = part[1] === "desc" ? -1 : 1;
+  }
   try {
     const user = req.user;
-    await user.populate("tasks").execPopulate();
+    await user
+      .populate({
+        path: "tasks",
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+          sort,
+        },
+      })
+      .execPopulate();
     res.send(req.user.tasks);
   } catch (e) {
     res.status(500).send();
